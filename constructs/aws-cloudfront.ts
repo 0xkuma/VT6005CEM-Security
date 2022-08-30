1894099886df8986213d9caf93f54d2741c6f47d
Eddie Cheung <eddie.cheung@ecloudvalley.com>
2022-08-31 01:01:37 +0800

    Add defaultObject param

--- constructs/aws-cloudfront.ts
+++ constructs/aws-cloudfront.ts
@@ -3,12 +3,14 @@ import { cloudfront } from '@cdktf/provider-aws';
 
 export interface AwsCloudFrontProps {
   readonly enabled: boolean;
+  readonly defaultRootObject: string;
   readonly defaultCacheBehavior: cloudfront.CloudfrontDistributionDefaultCacheBehavior;
   readonly origin: cloudfront.CloudfrontDistributionOrigin[];
   readonly restrictions: cloudfront.CloudfrontDistributionRestrictions;
   readonly viewerCertificate: cloudfront.CloudfrontDistributionViewerCertificate;
   readonly priceClass: 'PriceClass_100' | 'PriceClass_200' | 'PriceClass_All';
   readonly tags?: { [key: string]: string };
+  // readonly route53: cloudfront.CloudfrontDistribution;
 }
 
 export class AwsCloudfront extends Construct {
@@ -17,6 +19,7 @@ export class AwsCloudfront extends Construct {
 
     const {
       enabled,
+      defaultRootObject,
       defaultCacheBehavior,
       origin,
       restrictions,
@@ -27,6 +30,7 @@ export class AwsCloudfront extends Construct {
 
     new cloudfront.CloudfrontDistribution(this, 'distribution', {
       enabled,
+      defaultRootObject,
       defaultCacheBehavior,
       origin,
       restrictions,
