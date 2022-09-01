18621435d4b86121852b7f58e1f21dcae67e830d
Eddie Cheung <eddie.cheung@ecloudvalley.com>
2022-09-02 00:21:41 +0800

    Update addRoute53Record fn

--- constructs/aws-route53.ts
+++ constructs/aws-route53.ts
@@ -11,49 +11,41 @@ interface IRecordSet {
 export interface AwsRoute53Props {
   readonly name: string;
   readonly records: IRecordSet[];
+  readonly forceDestroy?: boolean;
   readonly tags?: { [key: string]: string };
 }
 
 export class AwsRoute53 extends Construct {
   public readonly zone: route53.Route53Zone;
-  public readonly addRoute53Record: (record: IRecordSet) => void;
+  public readonly addRoute53Record: (
+    record: IRecordSet,
+    resource: string,
+    num: number | string,
+  ) => void;
 
   constructor(scope: Construct, id: string, props: AwsRoute53Props) {
     super(scope, id);
 
-    const { name, records, tags } = props;
+    const { name, records, forceDestroy, tags } = props;
 
-    // create addRoute53Record function to add a record to a record set
-    this.addRoute53Record = (record: IRecordSet) => {
-      switch (record.type) {
-        case 'A':
-          new route53.Route53Record(this, `${record.records}-${record.name}-recordSet`, {
-            zoneId: this.zone.id,
-            name: record.name,
-            type: 'A',
-            ttl: 60,
-            records: record.records,
-          });
-          break;
-        case 'CNAME':
-          new route53.Route53Record(this, `${record.records}-${record.name}-recordSet`, {
-            zoneId: this.zone.id,
-            name: record.name,
-            type: 'CNAME',
-            ttl: 60,
-            records: record.records,
-          });
-          break;
-      }
+    this.addRoute53Record = (record: IRecordSet, resource: string, num: number | string) => {
+      new route53.Route53Record(this, `${resource}-${record.type}-recordSet-${num}`, {
+        zoneId: this.zone.zoneId,
+        name: record.name,
+        type: record.type,
+        ttl: record.ttl,
+        records: record.records,
+      });
     };
 
     this.zone = new route53.Route53Zone(this, 'zone', {
       name: name,
+      forceDestroy: forceDestroy || false,
       tags: tags || {},
     });
 
-    for (let record of records) {
-      this.addRoute53Record(record);
+    for (let record in records) {
+      this.addRoute53Record(records[record], 'route53', record);
     }
   }
 }
