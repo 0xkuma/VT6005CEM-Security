d63a8c9783de34ce4810525f84ccd8078284a2ac
Eddie Cheung <eddie.cheung@ecloudvalley.com>
2022-09-02 00:21:22 +0800

    Add cloudfront update bucket policy

--- constructs/aws-cloudfront.ts
+++ constructs/aws-cloudfront.ts
@@ -1,5 +1,5 @@
 import { Construct } from 'constructs';
-import { cloudfront } from '@cdktf/provider-aws';
+import { cloudfront, datasources, s3 } from '@cdktf/provider-aws';
 
 export interface AwsCloudFrontProps {
   readonly enabled: boolean;
@@ -11,9 +11,11 @@ export interface AwsCloudFrontProps {
   readonly priceClass: 'PriceClass_100' | 'PriceClass_200' | 'PriceClass_All';
   readonly tags?: { [key: string]: string };
   // readonly route53: cloudfront.CloudfrontDistribution;
+  readonly bucket: s3.S3Bucket;
 }
 
 export class AwsCloudfront extends Construct {
+  public readonly distribution: cloudfront.CloudfrontDistribution;
   constructor(scope: Construct, id: string, props: AwsCloudFrontProps) {
     super(scope, id);
 
@@ -21,14 +23,56 @@ export class AwsCloudfront extends Construct {
       enabled,
       defaultRootObject,
       defaultCacheBehavior,
-      origin,
       restrictions,
       viewerCertificate,
       priceClass,
       tags,
+      bucket,
     } = props;
 
-    new cloudfront.CloudfrontDistribution(this, 'distribution', {
+    const accountId = new datasources.DataAwsCallerIdentity(scope, 'account-id').accountId;
+
+    const oai = new cloudfront.CloudfrontOriginAccessIdentity(this, 'origin-access-identity', {});
+    new s3.S3BucketPolicy(this, 'bucket-policy', {
+      bucket: bucket.id,
+      policy: `{
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Sid": "AllowCloudFrontServicePrincipalReadOnly",
+                "Effect": "Allow",
+                "Principal": {
+                    "Service": "cloudfront.amazonaws.com"
+                },
+                "Action": "s3:GetObject",
+                "Resource": "${bucket.arn}/*",
+                "Condition": {
+                    "StringEquals": {
+                        "AWS:SourceArn": "arn:aws:cloudfront::${accountId}:distribution/${oai.id}"
+                    }
+                }
+            },
+            {
+                "Sid": "AllowLegacyOAIReadOnly",
+                "Effect": "Allow",
+                "Principal": {
+                    "AWS": "arn:aws:iam::cloudfront:user/CloudFront Origin Access Identity ${oai.id}"
+                },
+                "Action": "s3:GetObject",
+                "Resource": "${bucket.arn}/*"
+            }
+        ]
+      }`,
+    });
+
+    const origin = props.origin.map((o) => ({
+      ...o,
+      s3OriginConfig: {
+        originAccessIdentity: oai.cloudfrontAccessIdentityPath,
+      },
+    }));
+
+    this.distribution = new cloudfront.CloudfrontDistribution(this, 'distribution', {
       enabled,
       defaultRootObject,
       defaultCacheBehavior,
