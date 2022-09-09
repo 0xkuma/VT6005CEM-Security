55f34f808609c24c19af29b19f2be46e792f9205
Eddie Cheung <eddie.cheung@ecloudvalley.com>
2022-09-09 23:05:55 +0800

    REBUILD create booking record function

--- lambda/createBookingRecord.ts
+++ lambda/createBookingRecord.ts
@@ -4,37 +4,29 @@ import httpHeaderNormalizer from '@middy/http-header-normalizer';
 import httpJsonBodyParser from '@middy/http-json-body-parser';
 import httpErrorHandler from '@middy/http-error-handler';
 import { api } from './lib/api';
-import { isExistRecord, addDataToDynamoDB, getDateFromDynamoDB } from './lib/dynamodb';
+import { isExistRecord, addDataToDynamoDB, getDateFromDynamoDB } from './lib/ddbController';
 import { hashValue, encryptValue } from './lib/crypto';
 import { BOOKING_TABLE, TIME_SLOT_TABLE } from './config';
+import { sendEmail } from './lib/sesController';
 
 const originalHandler = async (event: any) => {
-  const { hkid, e_name, slot_date } = event.body;
-  const h_hkid = hashValue(hkid);
-  if (await isExistRecord(BOOKING_TABLE, { h_hkid })) {
-    return api(400, { message: 'HKID already exist' }, {});
-  }
-  const currDateStatus = await getDateFromDynamoDB(TIME_SLOT_TABLE, { slot_date }, [
-    'isHasVacant',
-    'total',
-  ]);
-  if (currDateStatus.Item == undefined || currDateStatus.Item.isHasVacant) {
-    let vancantNum = currDateStatus.Item == undefined ? 0 : currDateStatus.Item.total;
-    const bookingRes = await addDataToDynamoDB(BOOKING_TABLE, {
-      h_hkid: h_hkid,
-      e_name: encryptValue(h_hkid, e_name),
-      slot_date: slot_date,
-    });
-    const timeSlotRes = await addDataToDynamoDB(TIME_SLOT_TABLE, {
-      slot_date: slot_date,
-      total: vancantNum + 1,
-      isHasVacant: vancantNum + 1 < 10,
-    });
-    if (
-      bookingRes.$metadata.httpStatusCode === 200 &&
-      timeSlotRes.$metadata.httpStatusCode === 200
-    ) {
-      return api(200, { message: 'Add Record Success' }, {});
+  const { email, e_name, c_name, type, booking_date, slot, location } = event.body;
+  const en_email = encryptValue(hashValue(email), email);
+  const en_e_name = encryptValue(hashValue(email), e_name);
+  const en_c_name = encryptValue(hashValue(email), c_name);
+  const res = await addDataToDynamoDB(BOOKING_TABLE, {
+    email: en_email,
+    e_name: en_e_name,
+    c_name: en_c_name,
+    type: type,
+    booking_slot: `${booking_date}#${slot}`,
+    location: location,
+    confirmed: false,
+  });
+  if (res.$metadata.httpStatusCode === 200) {
+    const res = await sendEmail(email, 'Booking Confirmation', 'Your booking has been confirmed');
+    if (res.$metadata.httpStatusCode === 200) {
+      return api(200, { message: 'Success' }, {});
     }
   }
   return api(500, { message: 'Add Record Failed' }, {});
@@ -59,7 +51,15 @@ handler(
     multiValueQueryStringParameters: {},
     pathParameters: {},
     stageVariables: null,
-    body: JSON.stringify({ hkid: '123456', e_name: 'John', slot_date: '2021-01-01' }),
+    body: JSON.stringify({
+      email: 'cheungkamhung1998@gmail.com',
+      e_name: 'John',
+      c_name: 'John',
+      type: '1',
+      booking_date: '2021-01-01',
+      slot: '1',
+      location: 'Wan Chai',
+    }),
     isBase64Encoded: false,
   },
   {
