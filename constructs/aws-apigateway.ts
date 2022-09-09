df73ae46f5620ad5463652ffcbb8b201bb7dbc35
Eddie Cheung <eddie.cheung@ecloudvalley.com>
2022-09-10 00:42:46 +0800

    ADD api domain name

--- constructs/aws-apigateway.ts
+++ constructs/aws-apigateway.ts
@@ -1,5 +1,7 @@
 import { Construct } from 'constructs';
 import { apigateway } from '@cdktf/provider-aws';
+import { AwsAcmCertificate } from './aws-acm';
+import { AwsRoute53 } from './aws-route53';
 
 interface IEndpointConfiguration {
   readonly types: string[];
@@ -9,6 +11,9 @@ export interface AwsApiGatewayProps {
   readonly name: string;
   readonly description?: string;
   readonly endpointConfiguration?: IEndpointConfiguration;
+  readonly domanName: string;
+  readonly aws_acm: AwsAcmCertificate;
+  readonly aws_route53: AwsRoute53;
 }
 
 export class AwsApiGateway extends Construct {
@@ -37,7 +42,7 @@ export class AwsApiGateway extends Construct {
 
   constructor(scope: Construct, id: string, props: AwsApiGatewayProps) {
     super(scope, id);
-    const { name, description, endpointConfiguration } = props;
+    const { name, description, endpointConfiguration, domanName, aws_acm, aws_route53 } = props;
 
     this.api = new apigateway.ApiGatewayRestApi(this, `${name}-rest-api`, {
       name: name,
@@ -54,6 +59,22 @@ export class AwsApiGateway extends Construct {
         pathPart: pathPart,
       });
     };
+
+    const apiDomainName = new apigateway.ApiGatewayDomainName(this, `${name}-domain-name`, {
+      certificateArn: aws_acm.certificate.arn,
+      domainName: `api.${domanName}`,
+    });
+
+    aws_route53.addRoute53Record(
+      {
+        name: apiDomainName.domainName,
+        type: 'CNAME',
+        ttl: 60,
+        records: [apiDomainName.cloudfrontDomainName],
+      },
+      'api-gateway',
+      '0',
+    );
 
     this.apiGatewayMethod = (
       name: string,
@@ -86,10 +107,17 @@ export class AwsApiGateway extends Construct {
     };
 
     this.apiGatewayDeployment = (name: string, stageName: string) => {
-      return new apigateway.ApiGatewayDeployment(this, `${name}-deployment`, {
+      const depolyment = new apigateway.ApiGatewayDeployment(this, `${name}-deployment`, {
         restApiId: this.api.id,
         stageName: stageName,
       });
+      new apigateway.ApiGatewayBasePathMapping(this, `${name}-base-path-mapping`, {
+        apiId: this.api.id,
+        stageName: stageName,
+        domainName: apiDomainName.domainName,
+        dependsOn: [depolyment],
+      });
+      return depolyment;
     };
   }
 }
